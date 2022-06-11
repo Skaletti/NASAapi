9de5e27d48b75947cf6bf7fefe747c867bb684d3
Skaletti <gordey.korolev.2016@mail.ru>
2022-06-11 14:40:13 +0700

    update js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ searchButton.addEventListener("click", () => {
 
 async function sendApiRequest(clickCount) {
     const APIKEY = "Kxxlkd2hUzVR8tw66hpQWLNdXHG8WAVaE7iZaemg"
-    let response = await (await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=${APIKEY}`)).json();
+    let response = await (await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/Curiosity/photos?sol=1000&page=1&api_key=${APIKEY}`)).json();
     console.log(response)
     useApiData(response,clickCount)
-    // console.log(clickCount)
+    // console.log(clickCount)  Curiosity, Opportunity, and Spirit
 }
 
 function parseCamera(camera) {
@@ -21,8 +21,16 @@ function parseCamera(camera) {
 }
 
 async function useApiData(response,clickCount) {
-    document.querySelector("#content").innerHTML += `<img src=${response.photos[clickCount].img_src}>`
-    document.querySelector("#content").innerHTML += `<span>${response.photos[clickCount].id}</>`
-    document.querySelector("#content").innerHTML += `<p>${response.photos[clickCount].sol}</>`
-    document.querySelector("#content").innerHTML += `<p>${parseCamera(response.photos[clickCount].camera)}</>`
+
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `<img src=${response.photos[clickCount].img_src}>` 
+
+    myDiv = document.getElementById("content");
+    document.body.insertAdjacentElement(myDiv, newDiv);
+
+    // document.querySelector("#content").innerHTML += `<img src=${response.photos[clickCount].img_src}>`
+    // document.querySelector("#content").innerHTML += `<span>${response.photos[clickCount].id}</>`
+    // document.querySelector("#content").innerHTML += `<p>${response.photos[clickCount].sol}</>`
+    // document.querySelector("#content").innerHTML += `<p>${parseCamera(response.photos[clickCount].camera)}</>`
+    
 }
